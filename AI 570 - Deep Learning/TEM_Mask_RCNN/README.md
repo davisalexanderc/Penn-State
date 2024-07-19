3e7b2c1e610f0373135c6bd89106442cb3d40ad1
Alex Davis, PhD <106837639+davisalexanderc@users.noreply.github.com>
2024-07-19 17:10:28 -0400

    Created programs to download images from segments.ai and sort them into train, test, and val folders

---
+++ AI 570 - Deep Learning/TEM_Mask_RCNN/README.md
@@ -0,0 +1,5 @@
+data/: Store your dataset files.
+notebooks/: Store your Jupyter notebooks.
+src/: Store your source code.
+models/: Store your trained models.
+outputs/: Store the results, such as logs and figures.
---
+++ AI 570 - Deep Learning/TEM_Mask_RCNN/Segments_Json_to_COCO.py
@@ -0,0 +1,21 @@
+# pip install tensorflow
+# pip install segments-ai
+# pip install pycocotools
+
+import sys
+import os
+import tensorflow as tf
+import torch
+from segments import SegmentsClient, SegmentsDataset
+from segments.utils import export_dataset
+
+# Initialize a SegmentsDataset from the release file
+API_folder = "C:/Users/alex/Desktop/API_keys/"
+API_file = "Segments_API.txt"
+api_key = open(API_folder + API_file, "r").readline().strip()
+client = SegmentsClient(api_key)
+release = client.get_release('davis_alexander/TEM_Project3', 'TEM_V01')   # Alternatively: release = 'flowers-v1.0.json'
+dataset = SegmentsDataset(release, labelset='ground-truth', filter_by=['labeled', 'reviewed'])
+
+# Export to COCO panoptic format
+export_dataset(dataset, export_format='coco-instance')
